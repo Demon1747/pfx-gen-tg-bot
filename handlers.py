61e0a0888773dbafb675c6b8688e3da58b107209
Demon1747 <specdok117@mail.ru>
2025-10-30 09:00:13 +0300

    Added states to bot

--- handlers.py
+++ handlers.py
@@ -3,6 +3,9 @@ from aiogram import Router
 from aiogram.filters import Filter, CommandStart, Command
 from aiogram.types import Message
 
+from aiogram.fsm.state import StatesGroup, State
+from aiogram.fsm.context import FSMContext
+
 from config import settings
 
 
@@ -30,13 +33,64 @@ user_router.message.filter(
 )
 
 
+# Список состояний
+class PfxParameters(StatesGroup):
+    """ Состояния, отражающие процесс заполнения параметров для pfx """
+    cn = State()
+    cont_name = State()
+    keysize = State()
+    purpose = State()
+    is_exportable = State()
+    file_name = State()
+
+
 @default_router.message(CommandStart())
-async def user_start_cmd(message: Message):
+async def user_start_cmd(msg: Message):
     """ Команда для всех: позволяет получить id пользователя """
-    await message.answer(f'Приветствую, {message.from_user.full_name}! Твой id: {message.from_user.id}')
+    await msg.answer(f'Приветствую, {msg.from_user.full_name}! Твой id: {msg.from_user.id}')
 
 
 @user_router.message(Command('gen_pfx'))
-async def user_gen_pfx(message: Message):
+async def user_gen_pfx(msg: Message, state: FSMContext):
     """ Команда для начала процесса генерации pfx """
-    await message.answer(f'Lets go')
+    await state.set_state(PfxParameters.cn)
+    await msg.answer('Введите Common Name для сертификата')
+
+
+@user_router.message(PfxParameters.cn)
+async def get_pfx_cn(msg: Message, state: FSMContext):
+    """ Получение у пользователя CN сертификата """
+    await state.update_data(cn=msg.text)
+    await state.set_state(PfxParameters.cont_name)
+    await msg.answer('Введите название для ключевого контейнера')
+
+
+@user_router.message(PfxParameters.cont_name)
+async def get_pfx_cn(msg: Message, state: FSMContext):
+    """ Получение у пользователя названия контейнера """
+    await state.update_data(cn=msg.text)
+    await state.set_state(PfxParameters.purpose)
+    await msg.answer(text='Выберите длину открытого ключа')
+
+
+@user_router.message(PfxParameters.purpose)
+async def get_pfx_cn(msg: Message, state: FSMContext):
+    """ Определение назначения ключа с помощью клавиатуры """
+    await state.set_state(PfxParameters.is_exportable)
+    await msg.answer(text='Выберите назначение ключа')
+
+
+@user_router.message(PfxParameters.is_exportable)
+async def get_pfx_cn(msg: Message, state: FSMContext):
+    """ Определение экспортируемости ключа с помощью клавиатуры"""
+    await state.set_state(PfxParameters.file_name)
+    await msg.answer(text='Введите название файла для pfx-контейнера')
+
+
+@user_router.message(PfxParameters.file_name)
+async def get_pfx_cn(msg: Message, state: FSMContext):
+    """ Окончательная генерация pfx """
+    await state.update_data(file_name=msg.text)
+    await state.clear()
+
+    await msg.answer(text='Успех!')
